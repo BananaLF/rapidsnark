7be07b1e08edb2a843508f8525f82d7aabb546b9
fei.li <fei.li@okg.com>
2024-12-16 09:57:24 +0800

    rm nacos-sdk-cpp

--- nacos-sdk-cpp/src/naming/beat/BeatInfo.cpp
+++
@@ -1,9 +0,0 @@
-#include "src/json/JSON.h"
-#include "BeatInfo.h"
-#include "NacosString.h"
-
-namespace nacos{
-NacosString BeatInfo::toString() {
-    return JSON::toJSONString(*this);
-}
-}//namespace nacos
